eae52fb21dae3653f34b26bf8cdf8369b6bcd3f7
Vinoy Kumar <v685958@gmail.com>
2026-09-01 15:58:00 -0700

    Finish OAMDMA transfer and cpu halting

--- Bus.cpp
+++ Bus.cpp
@@ -2,14 +2,14 @@
 #include <array>
 #include "Bus.h"
 
-
-
 using namespace std;
 
-Bus::Bus(const string& filename){
-    //clear the ram
+Bus::Bus(const string &filename)
+{
+    // clear the ram
 
-    for(auto &data : ram){
+    for (auto &data : ram)
+    {
         data = 0x00;
     }
 
@@ -18,107 +18,117 @@ Bus::Bus(const string& filename){
     ppub = new PPUBus();
     cart->ConnectBus(this);
     witcpu->ConnectBus(this);
-    
-
 }
 
-//return type goes before the scope 
-void Bus:: write(uint16_t addr, uint8_t data){
-    //important range guard
-    if(addr >= 0x0000 && addr <= 0xffff){
-        //read from the internal ram or its mirrors
-        if(addr >= 0x0000 && addr <= 0x07ff){
-             ram[addr] = data;
+// return type goes before the scope
+void Bus::write(uint16_t addr, uint8_t data)
+{
+    // important range guard
+    if (addr >= 0x0000 && addr <= 0xffff)
+    {
+        // read from the internal ram or its mirrors
+        if (addr >= 0x0000 && addr <= 0x07ff)
+        {
+            ram[addr] = data;
         }
 
-        else if((addr >= 0x0800 && addr <= 0x0fff)) {
+        else if ((addr >= 0x0800 && addr <= 0x0fff))
+        {
             ram[addr - 0x800] = data;
         }
 
-        else if((addr >= 0x1000 && addr <= 0x17ff)) {
+        else if ((addr >= 0x1000 && addr <= 0x17ff))
+        {
             ram[addr - 0x1000] = data;
         }
 
-        else if((addr >= 0x1800 && addr <= 0x1fff)) {
+        else if ((addr >= 0x1800 && addr <= 0x1fff))
+        {
             ram[addr - 0x1800] = data;
         }
 
-        else if(addr >= 0x2000 && addr <= 0x3fff){
-             ppub->write_register(addr, data, witcpu->cputicks);
-        }
-
-        //oamdma
-        else if(addr == 0x4014){
+        else if (addr >= 0x2000 && addr <= 0x3fff)
+        {
             ppub->write_register(addr, data, witcpu->cputicks);
-
-            witcpu->dma_remaining = ((witcpu->cputicks + witcpu->lookup[witcpu->opcode].cycles) % 2 == 0)?514:513;
         }
 
+        // oamdma
+        else if (addr == 0x4014)
+        {
+            ppub->write_register(addr, data, witcpu->cputicks);
+            uint16_t page = data << 8;
+            uint8_t val;
 
+            // DMA transfer 256 bytes
+            for (int i = 0; i < 256; i++)
+            {
+                val = read(page);
+                ppub->oam[i] = val;
+                page += 1;
+            }
+            witcpu->dma_remaining = ((witcpu->cputicks + witcpu->lookup[witcpu->opcode].cycles) % 2 == 0) ? 514 : 513;
+        }
     }
-
-
-
 }
 
-uint8_t Bus:: read(uint16_t addr, bool bReadonly){
-    //Check if address in the 64kb range
-    if(addr >= 0x0000 && addr <= 0xffff){ //important range guard
+uint8_t Bus::read(uint16_t addr, bool bReadonly)
+{
+    // Check if address in the 64kb range
+    if (addr >= 0x0000 && addr <= 0xffff)
+    { // important range guard
 
-        //read from the internal ram or its mirrors
-        if(addr >= 0x0000 && addr <= 0x07ff){
+        // read from the internal ram or its mirrors
+        if (addr >= 0x0000 && addr <= 0x07ff)
+        {
             return ram[addr];
         }
 
-        else if((addr >= 0x0800 && addr <= 0x0fff)) {
+        else if ((addr >= 0x0800 && addr <= 0x0fff))
+        {
             return ram[addr - 0x800];
         }
 
-        else if((addr >= 0x1000 && addr <= 0x17ff)) {
+        else if ((addr >= 0x1000 && addr <= 0x17ff))
+        {
             return ram[addr - 0x1000];
         }
 
-        else if((addr >= 0x1800 && addr <= 0x1fff)) {
+        else if ((addr >= 0x1800 && addr <= 0x1fff))
+        {
             return ram[addr - 0x1800];
         }
 
-
-        //TODO: read from NES PPU registers region and mirrors
-        else if(addr >= 0x2000 && addr <= 0x3fff){
+        // read from NES PPU registers region and mirrors
+        else if (addr >= 0x2000 && addr <= 0x3fff)
+        {
             return ppub->read_register(addr);
         }
 
-        //TODO: NES APU and I/O register regions
-        //TODO: NES APU fun. that is disabled 
+        // TODO: NES APU and I/O register regions
+        // TODO: NES APU fun. that is disabled
 
+        // CARTRIDGE REGIONS
+        // 0x4020 - 0x5fff (no use ???)
+        // TODO: 0x6000 - 0x7fff prg_ram region.
 
-        //CARTRIDGE REGIONS
-        //0x4020 - 0x5fff (no use ???)
-        //TODO: 0x6000 - 0x7fff prg_ram region.
-
-        //prg_rom region
-        else if(addr >= 0x8000 && addr <= 0xffff){
-            //debug
-            // cout << "BUS TRIED READING FROM PRG_ROM" << '\n';
+        // prg_rom region
+        else if (addr >= 0x8000 && addr <= 0xffff)
+        {
+            // debug
+            //  cout << "BUS TRIED READING FROM PRG_ROM" << '\n';
             return cart->cpu_read(addr);
         }
 
-        else{
-             cout << "ERROR OCCURED. OUT OF ANY ADDRESS REGIONS" << endl;
+        else
+        {
+            cout << "ERROR OCCURED. OUT OF ANY ADDRESS REGIONS" << endl;
             return 0;
-           
         }
-
-
     }
 
-    else{
+    else
+    {
         cout << "ADDR out of 64kb" << endl;
         return 0x00;
     }
 }
-
-
-
-
-
